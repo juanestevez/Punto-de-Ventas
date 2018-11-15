a0178dd31215985730179859e9ba69669d8e6e40
Juan Manuel Estévez <juanestevez@gmail.com>
2018-11-15 17:08:26 -0600

    Se agregan librerías para conexión a base de datos.

---
+++ Punto de ventas/Connection/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using LinqToDB.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto_de_ventas.Connection
+{
+    public class Conexion : DataConnection
+    {
+        public Conexion() : base("dbVentas") { }
+    }
+}
--- Punto de ventas/frmPrincipal.cs
+++ Punto de ventas/frmPrincipal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Punto_de_ventas.Connection;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,7 @@ namespace Punto_de_ventas
         public FrmPrincipal()
         {
             InitializeComponent();
+            new Conexion();
         }
     }
 }
